d38d4b83866b7a984f08b6c4ce56093f2dfd0c04
liudaochun1219 <liudaochun1219@gmail.com>
2025-10-29 21:08:11 +0800

    Implement user login with password hashing

--- Global Kitchen/Login.aspx.cs
+++ Global Kitchen/Login.aspx.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace Global_Kitchen
 {
@@ -11,7 +11,74 @@ namespace Global_Kitchen
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+        }
 
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+            this.Load += new EventHandler(Form_Submit);
+        }
+
+        private void Form_Submit(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            {
+                string username = Request.Form["username"] ?? Request.Form["UserName"];
+                string password = Request.Form["password"];
+
+                if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+                {
+                    Response.Write("<script>alert('Please enter both username and password.');</script>");
+                    return;
+                }
+
+                string connectionString = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    string query = "SELECT Password FROM [User] WHERE Username = @Username";
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@Username", username);
+                        object result = cmd.ExecuteScalar();
+
+                        if (result == null)
+                        {
+                            Response.Write("<script>alert('Invalid username or password.');</script>");
+                            return;
+                        }
+
+                        string storedHash = result.ToString();
+
+                        string enteredHash = HashPassword(password);
+
+                        if (enteredHash.Equals(storedHash))
+                        {
+                            Session["Username"] = username;
+                            Response.Write("<script>alert('Login successful! Redirecting to homepage...');window.location='Index.aspx';</script>");
+                        }
+                        else
+                        {
+                            Response.Write("<script>alert('Invalid username or password.');</script>");
+                        }
+                    }
+                }
+            }
+        }
+
+        private string HashPassword(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                StringBuilder builder = new StringBuilder();
+                foreach (byte b in bytes)
+                {
+                    builder.Append(b.ToString("x2"));
+                }
+                return builder.ToString();
+            }
         }
     }
 }
