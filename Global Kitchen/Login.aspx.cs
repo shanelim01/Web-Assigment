0e5ea2e680fc871abe682084efef6200b2ef96e4
liudaochun1219 <liudaochun1219@gmail.com>
2025-11-02 21:40:18 +0800

    Update Login.aspx.cs

--- Global Kitchen/Login.aspx.cs
+++ Global Kitchen/Login.aspx.cs
@@ -1,84 +1,45 @@
 using System;
-using System.Data.SqlClient;
-using System.Configuration;
-using System.Security.Cryptography;
-using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using System.Web.UI;
+using System.Web.UI.WebControls;
 
 namespace Global_Kitchen
 {
-    public partial class Login : System.Web.UI.Page
+    public partial class index : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-        }
-
-        protected override void OnInit(EventArgs e)
-        {
-            base.OnInit(e);
-            this.Load += new EventHandler(Form_Submit);
-        }
-
-        private void Form_Submit(object sender, EventArgs e)
-        {
-            if (IsPostBack)
+            if (!IsPostBack)
             {
-                string username = Request.Form["username"] ?? Request.Form["UserName"];
-                string password = Request.Form["password"];
-
-                if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+                if (Session["Username"] != null)
                 {
-                    Response.Write("<script>alert('Please enter both username and password.');</script>");
-                    return;
+                    string username = Session["Username"].ToString();
+
+                    lblWelcome.Text = "Welcome, " + username + "!";
+                    lblWelcome.Visible = true;
+
+                    lnkLogin.Visible = false;
+                    lnkAdmin.Visible = false;
+                    lnkProfile.Visible = true;
+                    btnLogout.Visible = true;
                 }
-
-                string connectionString = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
-                using (SqlConnection conn = new SqlConnection(connectionString))
+                else
                 {
-                    conn.Open();
-
-                    string query = "SELECT Password FROM [User] WHERE Username = @Username";
-                    using (SqlCommand cmd = new SqlCommand(query, conn))
-                    {
-                        cmd.Parameters.AddWithValue("@Username", username);
-                        object result = cmd.ExecuteScalar();
-
-                        if (result == null)
-                        {
-                            Response.Write("<script>alert('Invalid username or password.');</script>");
-                            return;
-                        }
-
-                        string storedHash = result.ToString();
-
-                        string enteredHash = HashPassword(password);
-
-                        if (enteredHash.Equals(storedHash))
-                        {
-                            Session["Username"] = username;
-                            Response.Write("<script>alert('Login successful! Redirecting to homepage...');window.location='Index.aspx';</script>");
-                        }
-                        else
-                        {
-                            Response.Write("<script>alert('Invalid username or password.');</script>");
-                        }
-                    }
+                    lblWelcome.Visible = false;
+                    lnkLogin.Visible = true;
+                    lnkAdmin.Visible = true;
+                    lnkProfile.Visible = false;
+                    btnLogout.Visible = false;
                 }
             }
         }
 
-        private string HashPassword(string password)
+        protected void btnLogout_Click(object sender, EventArgs e)
         {
-            using (SHA256 sha256 = SHA256.Create())
-            {
-                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
-                StringBuilder builder = new StringBuilder();
-                foreach (byte b in bytes)
-                {
-                    builder.Append(b.ToString("x2"));
-                }
-                return builder.ToString();
-            }
+            Session.Abandon();
+            Response.Redirect("Index.aspx");
         }
     }
 }
